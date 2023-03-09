98543f89393e781e7c5ca2797af762cbe6a0d178
Alfie213 <arturbogoratov@gmail.com>
2023-03-09 13:13:43 +0300

    Added modal window

--- todolist.php
+++ todolist.php
@@ -19,6 +19,32 @@
         }
     }
 ?>
+<div id="registration">
+    <div id="blackout">
+        <div id="window">
+            <p> Окно регистрации </p>
+            <p> Email: </p>
+            <form action='todolist.php' method='get'>
+            <input type="text" name="name" placeholder="Type new Name" value="name">
+            <!-- <p> Password: </p>
+            <input type="password" name="password" placeholder="Type your password"> -->
+
+            <button type='submit' name='change' value="">Change name</button>
+            <a href="#" class="close"> Закрыть окно </a>
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php
+    if(isset($_REQUEST['change']))
+    {
+        print("
+        <button>Button</button>
+        ");
+    }
+?>
+
 <div class="content">
     <form action="" method="get">
         <input type="text" name="tasks" id="tasks" placeholder="task">
@@ -55,7 +81,7 @@
                     }
 
                     print("
-                    <input type='submit' name='edit' value='".$result['id']."'> Редактировать
+                    <a href='#registration'> Редактировать </a>
                     <input type='submit' name='delete' value='".$result['id']."'> Удалить
                 
             </div>
