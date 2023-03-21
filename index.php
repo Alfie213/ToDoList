6679ef88b680e7127ba380f57075f17cfdf6472e
Alfie213 <arturbogoratov@gmail.com>
2023-03-21 08:08:23 +0300

    Some changes
    Draggable = true doesn't work.

--- index.php
+++ index.php
@@ -17,7 +17,9 @@ require('todolist.php');
 
 <div id="container">
     <div class="zone1"></div>
-    <div class="zone2"><img id="ufo" src="./ufo.png"></div>
+    <div class="zone2">
+        <img id="ufo" src="./ufo.png" draggable="true">
+    </div>
 </div>
 <script src="./drop.js"></script>
 
